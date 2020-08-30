ed816d8707716a724762bb0dc7ea60ebb852f2e2
Willians Nieves <williamsnieves@gmail.com>
2020-08-30 22:55:17 +0200

    finish sample for laser defender

---
+++ Laser defender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy stats")]
+    [SerializeField] float health = 100;
+    [SerializeField] int scoreValue = 150;
+
+    [Header("Shooting")]
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 10f;
+
+    [Header("Sound effects")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplotion = 1f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] [Range(0,1)] float deathSoundVolume = 0.75f;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.25f;
+    [SerializeField] AudioClip shootSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShotCounter();
+    }
+
+    private void CountDownAndShotCounter()
+    {
+        shotCounter -= Time.deltaTime;
+
+        if(shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+            projectile,
+            transform.position,
+            Quaternion.identity
+            ) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if(!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOfExplotion);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
+    }
+}
---
+++ Number Bases/Assets/BaseNumbers.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BaseNumbers : MonoBehaviour
+{
+    public int selectedNumber = 173;
+    public string binaryResult = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        var firstResult = selectedNumber / 2;
+        var firstRemainder = selectedNumber % 2;
+        var secondResult = firstResult / 2;
+        var secondRemainder = firstResult % 2;
+        var thirdResult = secondResult / 2;
+        var thirdRemainder = secondResult % 2;
+        var fourthResult = thirdResult / 2;
+        var fourthRemainder = thirdResult % 2;
+        var fifthResult = fourthResult / 2;
+        var fifthRemainder = fourthResult % 2;
+        var sixthResult = fifthResult / 2;
+        var sixthRemainder = fifthResult % 2;
+        var seventhResult = sixthResult / 2;
+        var seventhRemainder = sixthResult % 2;
+
+        binaryResult = firstRemainder.ToString()
+                       + secondRemainder.ToString()
+                       + thirdRemainder.ToString()
+                       + fourthRemainder.ToString()
+                       + fifthRemainder.ToString()
+                       + sixthRemainder.ToString()
+                       + seventhRemainder.ToString();
+
+        print("firstResult: " + firstResult);
+        print("firstRemainder: " + firstRemainder);
+        print("secondResult: " + secondResult);
+        print("secondRemainder: " + secondRemainder);
+        print("thirdResult: " + thirdResult);
+        print("thirdRemainder: " + thirdRemainder);
+        print("fourthResult: " + fourthResult);
+        print("fourthRemainder: " + fourthRemainder);
+        print("fifthResult: " + fifthResult);
+        print("fifthRemainder: " + fifthRemainder);
+        print("sixthResult: " + sixthResult);
+        print("sixthRemainder: " + sixthRemainder);
+        print("seventhResult: " + seventhResult);
+        print("seventhRemainder: " + seventhRemainder);
+
+        print("binaryResult: " + binaryResult);
+        print("Back to decimal: " + Convert.ToInt32(binaryResult, 2));
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
